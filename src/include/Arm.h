97f915c965a0d196efdd2325e3adf7d1aa44cd1e
mellw0101 <melwivensson@gmail.com>
2024-06-30 02:31:11 +0200

    Changes to be committed: 	modified:   src/include/Arm.h

--- src/include/Arm.h
+++ src/include/Arm.h
@@ -14,11 +14,11 @@ uintptr_t get_base_mem_addr(uintptr_t physical_addr);
 // #define RK3399_T_CLOCK_REG      (RK3399_T_BASE_ADDR + 0x00)
 // #define RK3399_T_CTRL_REG       (RK3399_T_BASE_ADDR + 0x04)
 // #define RK3399_T_DATA_REG       (RK3399_T_BASE_ADDR + 0x08)
-
+//
 // #define RK3399_T_ENABLE         0x01
 // #define RK3399_T_CHANNEL_SELECT 0x00 // Assuming channel 0 for simplicity
 // #define RK3399_T_CLOCK_DIV      0x00000003
-
+//
 // class RK3399_T
 // {
 // public:
@@ -27,7 +27,7 @@ uintptr_t get_base_mem_addr(uintptr_t physical_addr);
 //         writeRegister(RK3399_T_CLOCK_REG, RK3399_T_CLOCK_DIV);
 //         writeRegister(RK3399_T_CTRL_REG, RK3399_T_ENABLE | RK3399_T_CHANNEL_SELECT);
 //     }
-
+//
 //     s16
 //     readTemperature()
 //     {
@@ -35,20 +35,20 @@ uintptr_t get_base_mem_addr(uintptr_t physical_addr);
 //         s16 temperature = convertToTemperature(adc_value);
 //         return temperature;
 //     }
-
+//
 // private:
 //     void
 //     writeRegister(u32 reg, u32 value)
 //     {
 //         *(volatile u32 *)(uintptr_t)reg = value;
 //     }
-
+//
 //     u32
 //     readRegister(u32 reg)
 //     {
 //         return *(volatile u32 *)(uintptr_t)reg;
 //     }
-
+//
 //     s16
 //     convertToTemperature(s16 adc_value)
 //     {
@@ -123,7 +123,7 @@ private:
             throw std::runtime_error("Cannot open /dev/mem");
         }
 
-        mapped_base = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, mem_fd, base_addr & ~MAP_MASK);
+        mapped_base = mmap(0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, mem_fd, RK3399_T_BASE_ADDR & ~MAP_MASK);
         if (mapped_base == MAP_FAILED)
         {
             close(mem_fd);

0062d01ee61c66bef43ee3f33cdcb1aea4d896be
mellw0101 <melwivensson@gmail.com>
2024-06-30 11:57:57 +0200

    Changes to be committed: 	modified:   src/cpp/Arm.cpp 	modified:   src/include/Arm.h

--- src/cpp/Arm.cpp
+++ src/cpp/Arm.cpp
@@ -26,3 +26,16 @@ get_base_mem_addr(uintptr_t physical_addr)
 
     return reinterpret_cast<uintptr_t>(mapped_base) + (physical_addr & MAP_MASK);
 }
+
+uint32_t
+ddr_get_rate(void)
+{
+    uint32_t refdiv, postdiv1, fbdiv, postdiv2;
+
+    refdiv   = mmio_read_32(CRU_BASE + CRU_PLL_CON(DPLL_ID, 1)) & 0x3f;
+    fbdiv    = mmio_read_32(CRU_BASE + CRU_PLL_CON(DPLL_ID, 0)) & 0xfff;
+    postdiv1 = (mmio_read_32(CRU_BASE + CRU_PLL_CON(DPLL_ID, 1)) >> 8) & 0x7;
+    postdiv2 = (mmio_read_32(CRU_BASE + CRU_PLL_CON(DPLL_ID, 1)) >> 12) & 0x7;
+
+    return (24 / refdiv * fbdiv / postdiv1 / postdiv2) * 1000 * 1000;
+}

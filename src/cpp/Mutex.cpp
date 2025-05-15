fccb2632c790a7b2fbe9fc009d8054175af85cef
mellw0101 <melwivensson@gmail.com>
2025-05-15 15:42:37 +0200

    No msg

--- src/cpp/Mutex.cpp
+++ src/cpp/Mutex.cpp
@@ -1,29 +1,29 @@
 #include "../include/Mutex.h"
 
-void lock_pthread_mutex(pthread_mutex_t *mutex, bool lock) {
-  int result = (lock == TRUE) ? pthread_mutex_lock(mutex) : pthread_mutex_unlock(mutex);
-  if (result == 0) {
-    return;
-  }
-  switch (result) {
-    case EAGAIN : {
-      logE("Mutex exceeded the maximum number of recursive locks.");
-      break;
-    }
-    case EDEADLK : {
-      logE("Mutex caused a deadlock or was already owned by this thread.");
-      break;
-    }
-    case EINVAL : {
-      logE("Mutex is not valid, indicating a fatal error.");
-      break;
-    }
-    case EPERM : {
-      logE("Mutex was locked by another thread, or not at all.");
-      break;
-    }
-    default : {
-      logE("Mutex encountered an unexpected error code: %d.", result);
-    }
-  }
-}
+// void lock_pthread_mutex(pthread_mutex_t *mutex, bool lock) {
+//   int result = (lock == TRUE) ? pthread_mutex_lock(mutex) : pthread_mutex_unlock(mutex);
+//   if (result == 0) {
+//     return;
+//   }
+//   switch (result) {
+//     case EAGAIN : {
+//       logE("Mutex exceeded the maximum number of recursive locks.");
+//       break;
+//     }
+//     case EDEADLK : {
+//       logE("Mutex caused a deadlock or was already owned by this thread.");
+//       break;
+//     }
+//     case EINVAL : {
+//       logE("Mutex is not valid, indicating a fatal error.");
+//       break;
+//     }
+//     case EPERM : {
+//       logE("Mutex was locked by another thread, or not at all.");
+//       break;
+//     }
+//     default : {
+//       logE("Mutex encountered an unexpected error code: %d.", result);
+//     }
+//   }
+// }
--- src/include/Mutex.h
+++ src/include/Mutex.h
@@ -4,25 +4,25 @@
 /* Either lock or unlock the threadpools mutex, with full error reporting.  We
  * will not interviene on fail as I want to see for now atleast what it takes to
  * crash. */
-void lock_pthread_mutex(pthread_mutex_t *mutex, bool lock);
+// void lock_pthread_mutex(pthread_mutex_t *mutex, bool lock);
 
-/* RAII complient way to lock a pthread mutex.  This struct will lock
- * the mutex apon its creation, and unlock it when it goes out of scope. */
-struct pthread_mutex_guard_t {
-  pthread_mutex_t *m = nullptr;
-  explicit pthread_mutex_guard_t(pthread_mutex_t *mutex)
-      : m(mutex) {
-    if (m == nullptr) {
-      logE("A 'nullptr' was passed to 'pthread_mutex_guard_t'.");
-    }
-    else {
-      lock_pthread_mutex(m, true);
-    }
-  }
-  ~pthread_mutex_guard_t(void) {
-    if (m != nullptr) {
-      lock_pthread_mutex(m, false);
-    }
-  }
-  DEL_CM_CONSTRUCTORS(pthread_mutex_guard_t);
-};
+// /* RAII complient way to lock a pthread mutex.  This struct will lock
+//  * the mutex apon its creation, and unlock it when it goes out of scope. */
+// struct pthread_mutex_guard_t {
+//   pthread_mutex_t *m = nullptr;
+//   explicit pthread_mutex_guard_t(pthread_mutex_t *mutex)
+//       : m(mutex) {
+//     if (m == nullptr) {
+//       logE("A 'nullptr' was passed to 'pthread_mutex_guard_t'.");
+//     }
+//     else {
+//       lock_pthread_mutex(m, true);
+//     }
+//   }
+//   ~pthread_mutex_guard_t(void) {
+//     if (m != nullptr) {
+//       lock_pthread_mutex(m, false);
+//     }
+//   }
+//   DEL_CM_CONSTRUCTORS(pthread_mutex_guard_t);
+// };
